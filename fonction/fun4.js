8b98a4630fd8c318fa8a9ea50ef84a3e2965ca38
samira-2002-code <samiraassemlali2@gmail.com>
2025-10-17 17:55:01 +0100

    Refactor inverserTableau to use in-place swapping
    Refactor the array inversion function to use a two-pointer approach instead of creating a new array.

--- fonction/fun4.js
+++ fonction/fun4.js
@@ -1,11 +1,16 @@
-const prompt=require("prompt-sync")();
 function inverserTableau(tab){
-    let nouveuTableau=[];
-    for(let i=tab.length-1;i>=0;i--) {
-        nouveuxTableau.push(tab[i]);
-}
-return nouveuTableau;
+    let temp;
+     let start=0;
+     let end;
+     end=tab.length-1;
+     while (start<end) {
+        temp=tab[start];
+        tab[start]=tab[end]
+        tab[end]=temp;
+        start++;
+        end--;
+     }
+     return tab
 
 } 
 console.log(inverserTableau([15,16,17,18,19]));
-console.log(inverserTbableau (["c","d","e","f","g"]));
